f43cd33e56b7c41fe5ef526dba76f2ff5609c939
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2020-05-16 09:53:40 +0200

    Task builder

--- src/main/java/co/devfoundry/Main.java
+++ src/main/java/co/devfoundry/Main.java
@@ -1,5 +1,6 @@
 package co.devfoundry;
 
+import co.devfoundry.flight.FlightLeg;
 import co.devfoundry.house.House;
 
 public class Main {
@@ -9,16 +10,8 @@ public class Main {
 //        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
 //        House house2 = new House("")
 
-        House house2 = new House.HouseBuilder()
-                .buildDoors("doors")
-                .buildFloors("Floors")
-                .buildGarage("garage")
-                .buildRoof("roof")
-                .build();
-
-        System.out.println(house2);
-
-
+        FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas","Los Angeles").price(50).build();
+        System.out.println(leg);
     }
 
 }
